69774d982e8b70172b959ac6bc4e3f24c93c1e05
Cale Davis <daviscale@gmail.com>
2011-12-15 20:38:13 -0500

    order by status, then content

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   # GET /messages
   # GET /messages.json
   def index
-    @messages = Message.order("status").all
+    @messages = Message.order("status, content").all
 
     respond_to do |format|
       format.html # index.html.erb
